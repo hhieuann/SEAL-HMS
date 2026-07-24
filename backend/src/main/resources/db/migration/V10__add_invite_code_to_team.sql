6a6b2f9abd22295fa13e18238e6312fb38d58578
Nguyễn Minh Hoàng <hoangminh6112006@gmail.com>
2026-07-24 21:27:04 +0700

    Delete backend/src/main/resources/db/migration/V10__add_invite_code_to_team.sql

--- backend/src/main/resources/db/migration/V10__add_invite_code_to_team.sql
+++
@@ -1,3 +0,0 @@
--- Add invite_code column to team table for random 6-char join codes
-ALTER TABLE team ADD COLUMN invite_code VARCHAR(6);
-CREATE UNIQUE INDEX uq_team_invite_code ON team(invite_code);
